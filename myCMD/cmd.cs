45f0a5db4ccf15bbd2ac0865b4039441f2e7d5cc
bertrandF <bertrand.feuillerat@hotmail.fr>
2013-12-26 17:26:44 +0100

    Added handlers for enter and directions keys in console

--- myCMD/cmd.cs
+++ myCMD/cmd.cs
@@ -10,11 +10,62 @@ namespace myCMD
 {
     class Cmd : RichTextBox
     {
+        public bool CursorIsAtStartOfLine 
+        { 
+            get 
+            {
+                if (SelectionStart == Text.Length - Lines[Lines.Length - 1].Length)
+                    return true;
+                return false;
+            } 
+        }
+        public bool CursorIsAtEndOfLine
+        {
+            get
+            {
+                if (SelectionStart == Text.Length)
+                    return true;
+                return false;
+            }
+        }
+
         public Cmd() : base() {
             this.BackColor = Color.Black;
             this.ForeColor = Color.DarkGray;
             this.Font = new Font("windows_command_prompt.ttf", 10);
             this.Dock = DockStyle.Fill;
         }
+
+        protected override void OnKeyDown(KeyEventArgs e) 
+        {
+            switch(e.KeyCode) 
+            {
+                case Keys.Down:
+                case Keys.Up:
+                    // TODO: Historique !
+                    e.Handled = true;
+                    break;
+                case Keys.Left:
+                    if (CursorIsAtStartOfLine)
+                        e.Handled = true;
+                    break;
+                case Keys.Right:
+                    if (CursorIsAtEndOfLine)
+                        e.Handled = true;
+                    break;
+                case Keys.Enter:
+                    MessageBox.Show("Executing cmd : " + this.Lines[this.Lines.Length - 1]);
+                    Text += "\r\n";
+                    this.SelectionStart = Text.Length;
+                    e.Handled = true;
+                    break;
+                default:
+                    break;
+            }
+
+            base.OnKeyDown(e);
+        }
+
+
     }
 }
